b784ebe487248398f3c0d3d249bf09934068b1f5
RazvanGradinaru <razvan.gradinaru@yahoo.com>
2025-08-08 20:47:35 +0300

    basically nothing

--- app/page.tsx
+++ app/page.tsx
@@ -50,7 +50,7 @@ export default function Home() {
             Read our docs
           </a>
         </div>
-      </main>
+      </main> {/* end of main */}
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
